53ad35bf9c918eae6be92d0197c635014976649a
Shahin <32805117+Shahin-rmz@users.noreply.github.com>
2021-07-07 19:25:45 +0430

    Update pdf_to_html.py

--- pdf_to_html.py
+++ pdf_to_html.py
@@ -17,7 +17,7 @@ from natsort import natsorted
 # In[4]:
 
 
-htmltxt = ''
+htmltxt = ''                                         #converting PDF to xhtml
 htmlfile2 = open ('htmlfile2.html', 'w+')
 doc = fitz.open('govahi.pdf')
 for page in doc:
@@ -30,7 +30,7 @@ htmlfile2.close()
 # In[5]:
 
 
-file = open('htmlfile.html', 'r')
+file = open('htmlfile.html', 'r')                                #scraping the html file
 soup = BeautifulSoup(file.read(),'html.parser')
 l = []
 for item in soup.select('p[style *= "left:118pt"]'):
@@ -40,7 +40,7 @@ for item in soup.select('p[style *= "left:118pt"]'):
 # In[6]:
 
 
-df = pd.DataFrame(l)
+df = pd.DataFrame(l)                                          #converting the extracted data to csv and then pandas Data Frame
 df.to_csv('names.csv',index = False)
 
 
@@ -54,7 +54,7 @@ df2.columns = ['obj_name']
 # In[8]:
 
 
-fpath = '/home/shahin/3-ComputerProj/projects/pdf_extractor/images_test/'
+fpath = '/path/to/images_test/'                                          # images are in binary,we rename the scraped images to their name wich have alsobeen scraped and saved to Data Frame
 for file, i in zip(natsorted(os.listdir(fpath)),df2.obj_name.iloc) :
     ext = os.path.splitext(file)[-1].lower()
     if ext == '.png':
